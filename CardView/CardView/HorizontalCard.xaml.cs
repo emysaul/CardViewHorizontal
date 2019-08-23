be644382e7be9ccae30fd1b981acbeff997ef7a8
emysaul <emysaul@hotmail.es>
2019-08-23 17:18:56 -0400

    added project

---
+++ CardView/CardView/HorizontalCard.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CardView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HorizontalCard : ContentPage
+    {
+        public HorizontalCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
